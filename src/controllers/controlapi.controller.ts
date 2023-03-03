57a0d3dbd92ddff64b9c44eb958b3c4550109685
sixi <sixi>
2023-03-03 20:16:26 +0100

    initial commit

---
+++ src/controllers/controlapi.controller.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+
+class IndexController {
+  public index = (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      // res.sendStatus(200);
+      res
+        .json({
+          hello: 'World!',
+        })
+        .status(200);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default IndexController;
